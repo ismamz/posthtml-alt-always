5761b9cc8eb44872dc3dc0d4a689eb33f401f7ed
ismamz <ismael@isma.uy>
2016-01-18 15:38:34 -0300

    Fix empty tag

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var posthtml = require('posthtml');
 
 module.exports = function () {
 	return function altAlways(tree) {
-		tree.match({ tag: 'img', attrs: { alt: false }}, function(node) {
-			node.attrs.alt = true;
+		tree.match({ tag: 'img', attrs: { alt: false } }, function(node) {
+			node.attrs.alt = '';
 			return node;
 		});
 	};
